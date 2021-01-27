54ac031ad2e3f38d0cbc0bc64af4303d1b299b38
carl-33 <koepkecj@gmail.com>
2021-01-27 14:47:29 -0600

    updated resume

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../style/contact.css"
-import resume from "../assets/carl-resume.pdf"
+import resume from "../assets/Resume-Carl-Koepke.docx.pdf"
 
 function Contact() {
     return (
