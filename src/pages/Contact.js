07e04e0afb0375ee5f0c42844edd8fa0def8ce18
carl-33 <koepkecj@gmail.com>
2021-01-29 11:11:42 -0600

    update resume and fixed typos

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../style/contact.css"
-import resume from "../assets/Resume-Carl-Koepke.docx.pdf"
+import resume from "../assets/Resume-Carl-Koepke.pdf"
 
 function Contact() {
     return (
